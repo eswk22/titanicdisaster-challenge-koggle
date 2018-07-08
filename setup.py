8384a91a6e221bfb5a9b90d1317fa985eaf34abe
eswk22 <elixireswar@gmail.com>
2018-07-08 09:41:34 -0500

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Koggle chanllege for titanic disaster',
+    author='Eswaran Krishnamoorthy',
+    license='MIT',
+)
